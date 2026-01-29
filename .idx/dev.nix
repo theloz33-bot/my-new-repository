831bf9f384daee6aaeae6c5faf9c8a35e8b0240e
theloz33 <theloz33@gmail.com>
2026-01-29 15:03:57 +0000

    Initial commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,39 +7,32 @@
   packages = [
     pkgs.nodejs_20
     pkgs.python3
+    pkgs.gh
+    pkgs.gemini-cli
   ];
   # Sets environment variables in the workspace
   env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
-      "google.gemini-cli-vscode-ide-companion"
-    ];
-    # Enable previews and customize configuration
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
-          manager = "web";
-        };
-      };
-    };
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "style.css" "main.js" "index.html" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
-    };
+  # Fast way to run commands without running a full shell
+  scripts = {
+    # "hello" = "echo Hello World";
   };
+  # Binds a port from the VM to the editor
+  # To learn more, see https://developers.google.com/idx/guides/customize-idx-env#bind-a-port
+  ports = {};
+  # Enables the specified language's VS Code extension and SDK
+  languages = {
+    # "python" = {
+    #   "version" = "3.11";
+    # };
+  };
+  # These settings will be applied to the VS Code workspace .vscode/settings.json file
+  # To learn more, see https://developers.google.com/idx/guides/customize-idx-env#ide-settings
+  workspace = {
+    # "my-extension.my-setting" = "value";
+  };
+  # Installs the listed VS Code extensions.
+  # To learn more, see https://developers.google.com/idx/guides/customize-idx-env#vscode-extensions
+  extensions = [
+    # "vscode.extension-id"
+  ];
 }
